c762a956f903953b75fa56f96fcb01a4394194b7
DaniilSukhanov <danchopancho@icloud.com>
2022-01-31 20:06:38 +0300

    Исправление.

--- qt.py
+++ qt.py
@@ -1,9 +1,9 @@
 import sys
 
-import requests
-from PyQt5.QtGui import QPixmap
 from ui_file import Ui_MainWindow
 from PyQt5.QtWidgets import QApplication, QMainWindow
+
+from request import get_map_image
 
 SCREEN_SIZE = [1000, 600]
 
@@ -13,37 +13,15 @@ class Example(QMainWindow, Ui_MainWindow):
         super().__init__()
         self.setupUi(self)
         self.pushButton.clicked.connect(self.read_input)
-        self.PREFIX = 'https://static-maps.yandex.ru/1.x/?'
-        self.APIKEY = "40d1649f-0493-4b70-98ba-98533de7710b"
 
     def read_input(self):
         dolgota = float(self.lineEdit.text())
         shirote = float(self.lineEdit_3.text())
         mash = int(self.lineEdit_2.text())
-        if mash in range(0, 18) and dolgota >= -180 and dolgota <= 180 and shirote >= -90 and\
-                 shirote <= 90:
-            self.get_map_image(f'{dolgota},{shirote}', str(mash))
-
-    def get_map_image(self, ll, zoom, map_type='map'):
-        params = {
-            'll': ll,
-            'z': zoom,
-            'l': map_type
-        }
-        response = requests.get(self.PREFIX, params=params)
-        if not response:
-            print(
-                f"""Ошибка!
-                    Запрос: {self.PREFIX, params}
-                    HTTP статус: {response.status_code}; {response.reason}"""
-            )
-        map_file = 'map_file.png'
-        with open(map_file, "wb") as file:
-            file.write(response.content)
-        pixmap = QPixmap(map_file)
-        print('somet')
-        self.label.setPixmap(pixmap)
-
+        try:
+            self.label.setPixmap(get_map_image(f'{dolgota},{shirote}', mash))
+        except AssertionError:
+            self.label.setText('Некорректные данные!')
 
 
 if __name__ == '__main__':
--- request.py
+++ request.py
@@ -7,11 +7,14 @@ APIKEY = "40d1649f-0493-4b70-98ba-98533de7710b"
 
 
 def get_map_image(ll, zoom, map_type='map'):
+    ll_tuple = tuple(map(float, ll.split(',')))
+    assert -180 <= ll_tuple[0] <= 180
+    assert -90 <= ll_tuple[1] <= 90
+    assert zoom in range(18)
     params = {
         'll': ll,
         'z': zoom,
-        'l': map_type,
-        'key': APIKEY
+        'l': map_type
     }
     response = requests.get(PREFIX, params=params)
     pixmap = QPixmap()
