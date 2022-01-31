fa78c9a993ff70d1a14ccf2f02ff5aee20af2ee5
kmd4 <mdku2005@gmail.com>
2022-01-31 17:45:26 +0300

    commit3

--- qt.py
+++ qt.py
@@ -1,34 +1,43 @@
-import os
 import sys
 
 import requests
 from PyQt5.QtGui import QPixmap
-from PyQt5 import uic
-from PyQt5.QtWidgets import QApplication, QWidget
+from ui_file import Ui_MainWindow
+from PyQt5.QtWidgets import QApplication, QMainWindow
 
 SCREEN_SIZE = [1000, 600]
 
 
-
-class Example(QWidget):
+class Example(QMainWindow, Ui_MainWindow):
     def __init__(self):
         super().__init__()
-        uic.loadUi('dis.ui', self)
-        self.getImage()
+        self.setupUi(self)
         self.pushButton.clicked.connect(self.read_input)
+        self.PREFIX = 'https://static-maps.yandex.ru/1.x/?'
+        self.APIKEY = "40d1649f-0493-4b70-98ba-98533de7710b"
 
     def read_input(self):
         dolgota = self.lineEdit.text()
         shirote = self.lineEdit_3.text()
         mash = self.lineEdit_2.text()
-        if mash in range(0, 18) and float(dolgota) >= -180 and float(dolgota) <= 180 and float(shirote) >= -90 and\
-                float(shirote) <= 90:
-            pass #передача параметров в карту
+        print(3)
+        # if mash in range(0, 18) and float(dolgota) >= -180 and float(dolgota) <= 180 and float(shirote) >= -90 and\
+        #         float(shirote) <= 90:
+        self.get_map_image(f'{float(dolgota)},{float(shirote)}', int(mash))
 
-
-    def closeEvent(self, event):
-        """При закрытии формы подчищаем за собой"""
-        os.remove(self.map_file)
+    def get_map_image(self, ll, zoom, map_type='map'):
+        print(1)
+        params = {
+            'll': ll,
+            'z': zoom,
+            'l': map_type,
+            'key': self.APIKEY
+        }
+        response = requests.get(self.PREFIX, params=params)
+        pixmap = QPixmap()
+        pixmap.loadFromData(response.content)
+        self.label.setPixmap(pixmap)
+        print('something')
 
 
 if __name__ == '__main__':
