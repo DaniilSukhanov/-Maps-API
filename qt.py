4e650d9d487bc265e88536b6834e8ed78e3935cf
DaniilSukhanov <danchopancho@icloud.com>
2022-02-02 18:42:46 +0300

    Большая задача по Maps API. Часть №2

--- qt.py
+++ qt.py
@@ -1,27 +1,32 @@
 import sys
 
-from ui_file import Ui_MainWindow
+from dis import Ui_MainWindow
 from PyQt5.QtWidgets import QApplication, QMainWindow
 
 from request import get_map_image
-
-SCREEN_SIZE = [1000, 600]
 
 
 class Example(QMainWindow, Ui_MainWindow):
     def __init__(self):
         super().__init__()
         self.setupUi(self)
-        self.pushButton.clicked.connect(self.read_input)
+        self.pushButton_load_image.clicked.connect(self.read_input)
+
+    def wheelEvent(self, event):
+        """Управлением масштабом (колесо мыши)"""
+        if event.angleDelta().y() < 0:
+            self.spinBox_zoom.stepDown()
+        else:
+            self.spinBox_zoom.stepUp()
+        self.read_input()
 
     def read_input(self):
-        dolgota = float(self.lineEdit.text())
-        shirote = float(self.lineEdit_3.text())
-        mash = int(self.lineEdit_2.text())
-        try:
-            self.label.setPixmap(get_map_image(f'{dolgota},{shirote}', mash))
-        except AssertionError:
-            self.label.setText('Некорректные данные!')
+        dolgota = self.doubleSpinBox_latitude.value()
+        shirote = self.doubleSpinBox_longitude.value()
+        zoom = self.spinBox_zoom.value()
+        self.label_image.setPixmap(
+            get_map_image(f'{dolgota},{shirote}', zoom)
+        )
 
 
 if __name__ == '__main__':
