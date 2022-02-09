778b4759db8cb58e94df62cec4e3342e42043011
DaniilSukhanov <danchopancho@icloud.com>
2022-02-09 17:25:44 +0300

    Большая задача по Maps API. Часть №3

--- qt.py
+++ qt.py
@@ -2,6 +2,8 @@ import sys
 
 from dis import Ui_MainWindow
 from PyQt5.QtWidgets import QApplication, QMainWindow
+from PyQt5.QtGui import QKeyEvent, QWheelEvent
+from PyQt5.QtCore import Qt
 
 from request import get_map_image
 
@@ -11,21 +13,45 @@ class Example(QMainWindow, Ui_MainWindow):
         super().__init__()
         self.setupUi(self)
         self.pushButton_load_image.clicked.connect(self.read_input)
+        self.doubleSpinBox_latitude.valueChanged.connect(self.read_input)
+        self.doubleSpinBox_longitude.valueChanged.connect(self.read_input)
+        self.spinBox_zoom.valueChanged.connect(self.set_step_spinbox)
+        self.keys = {
+            Qt.Key_Up: self.doubleSpinBox_longitude.stepUp,
+            Qt.Key_Down: self.doubleSpinBox_longitude.stepDown,
+            Qt.Key_Left: self.doubleSpinBox_latitude.stepDown,
+            Qt.Key_Right: self.doubleSpinBox_latitude.stepUp
+        }
 
-    def wheelEvent(self, event):
+    def set_step_spinbox(self):
+        k = 2 ** self.spinBox_zoom.value()
+        spn_longitude = 180 / k
+        spn_latitude = 360 / k
+        self.doubleSpinBox_longitude.setSingleStep(spn_longitude)
+        self.doubleSpinBox_latitude.setSingleStep(spn_latitude)
+        self.read_input()
+
+    def wheelEvent(self, event: QWheelEvent):
         """Управлением масштабом (колесо мыши)"""
         if event.angleDelta().y() < 0:
             self.spinBox_zoom.stepDown()
         else:
             self.spinBox_zoom.stepUp()
-        self.read_input()
+
+    def keyPressEvent(self, event: QKeyEvent):
+        result = self.keys.get(event.key(), None)
+        if result is not None:
+            result()
 
     def read_input(self):
-        dolgota = self.doubleSpinBox_latitude.value()
-        shirote = self.doubleSpinBox_longitude.value()
-        zoom = self.spinBox_zoom.value()
+        latitude = self.doubleSpinBox_latitude.value()
+        longitude = self.doubleSpinBox_longitude.value()
         self.label_image.setPixmap(
-            get_map_image(f'{dolgota},{shirote}', zoom)
+            get_map_image(
+                f'{latitude},{longitude}',
+                f'{self.doubleSpinBox_latitude.singleStep()},'
+                f'{self.doubleSpinBox_longitude.singleStep()}'
+            )
         )
 
 
--- request.py
+++ request.py
@@ -1,18 +1,15 @@
 import requests
 from PyQt5.QtGui import QPixmap
+import const
 
 
-PREFIX = 'https://static-maps.yandex.ru/1.x/?'
-APIKEY = "40d1649f-0493-4b70-98ba-98533de7710b"
-
-
-def get_map_image(ll, zoom, map_type='map'):
+def get_map_image(ll, spn, map_type='map'):
     params = {
         'll': ll,
-        'z': zoom,
+        'spn': spn,
         'l': map_type
     }
-    response = requests.get(PREFIX, params=params)
+    response = requests.get(const.PREFIX, params=params)
     pixmap = QPixmap()
     pixmap.loadFromData(response.content)
     return pixmap
